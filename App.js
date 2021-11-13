9ec1ad561a002db7e0b63248d529f4ab66764f2c
Henrietha <areathy33@gmail.com>
2021-11-13 20:41:43 +0100

    Platform specific styling

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, SafeAreaView, Button, Alert } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  Alert,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Button
-        color="green"
+        color="black"
         title="click me"
         onPress={() =>
           Alert.prompt("Title", "message", (text) => console.log(text))
@@ -19,8 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: Platform.OS === "ios" ? "teal" : "violet",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
