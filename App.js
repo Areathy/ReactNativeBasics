8bed0e905f626a0e6c215785c5bb0c996b375805
Henrietha <areathy33@gmail.com>
2021-11-13 10:35:36 +0100

    Buttons

--- App.js
+++ App.js
@@ -1,45 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-} from "react-native";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello Universe!!!</Text>
-      {/* static image */}
-      <Image source={require("./assets/favicon.png")} />
-      {/* online image */}
-
-      <TouchableHighlight onPress={() => console.log("qwerty")}>
-        <Image
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-
-      <TouchableNativeFeedback>
-        <View
-          style={{
-            width: 100,
-            height: 50,
-            backgroundColor: "green",
-            padding: 10,
-          }}
-        ></View>
-      </TouchableNativeFeedback>
-
-      <StatusBar style="auto" />
+      <Button
+        color="green"
+        title="click me"
+        onPress={() => console.log("clicked")}
+      />
     </SafeAreaView>
   );
 }
